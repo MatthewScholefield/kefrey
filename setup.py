90edff7b7be27feef392782dbd9dfaba390ef53a
Matthew D. Scholefield <matthew331199@gmail.com>
2020-01-13 23:52:37 -0600

    Increment version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kefrey',
-    version='0.1.0',
+    version='0.1.1',
     description='A tool to track system-wide keypress frequencies',
     url='https://github.com/matthewscholefield/kefrey',
     author='Matthew D. Scholefield',
